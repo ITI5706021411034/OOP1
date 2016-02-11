0c8619e77107634492314043de78f95454862392
ITI5706021421034 <5706021421034@fitm.kmutnb.ac.th>
2016-02-11 23:35:44 +0700

    เขมินท์ ศิริขจร

--- Skocimis/src/Skocimis.java
+++ Skocimis/src/Skocimis.java
@@ -1,29 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 public class Skocimis {
 
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		int a1=0,b1=0,c1=0;
-		int wronga = 0;
-		int jup = 0 , spac = 0;
-		String number,str1="";
+		int a=0,b=0,c=0;
+		int wrong = 0;
+		int jump = 0 , spac = 0;
+		String num,str="";
 		
 		System.out.println("Enter number interger (0-100) fomat is (10 20 30)");
-		number=scan.nextLine();
-		for(int i = 0 ; i < number.length() ;i++){
-			if(number.substring(i,i+1).equals(" ")){spac++; str1=""; }
-			else { str1 += number.substring(i,i+1); 
-				if(spac == 0)a1=Integer.parseInt(str1);
-				else if(spac == 1)b1=Integer.parseInt(str1); 
-				else if(spac == 2)c1=Integer.parseInt(str1);
+		num=scan.nextLine();
+		for(int i = 0 ; i < num.length() ;i++){
+			if(num.substring(i,i+1).equals(" ")){spac++; str=""; }
+			else { str += num.substring(i,i+1); 
+				if(spac == 0)a=Integer.parseInt(str);
+				else if(spac == 1)b=Integer.parseInt(str); 
+				else if(spac == 2)c=Integer.parseInt(str);
 			}
 		}
-		if(a1<0||a1>99){System.out.println("Number error JingJO NO.1-");wronga=1;}
-		else if(b1<a1||b1>99){System.out.println("Number error JingJO NO.2-");wronga=1;}
-		else if(b1>c1||c1>99){System.out.println("Number error JingJO NO.3-");wronga=1;}
-			if(0<a1&&a1<b1&&b1<c1&&c1<100&&wronga==0){
-			jup= ((b1-a1)>=(c1-b1))?(b1-a1):(c1-b1);
-			System.out.println(jup-1);
+		if(a<0||a>99){System.out.println("Number error JingJO NO.1-");wrong=1;}
+		else if(b<a||b>99){System.out.println("Number error JingJO NO.2-");wrong=1;}
+		else if(b>c||c>99){System.out.println("Number error JingJO NO.3-");wrong=1;}
+			if(0<a&&a<b&&b<c&&c<100&&wrong==0){
+			jump = ((b-a)>=(c-b))?(b-a):(c-b);
+			System.out.println(jump-1);
 			}
 	}
 }
