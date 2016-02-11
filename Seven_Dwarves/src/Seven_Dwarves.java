ffd6c3b58e176a184c5faf9b13f33e75cceb89d3
ITI5706021421034 <5706021421034@fitm.kmutnb.ac.th>
2016-02-11 23:34:50 +0700

    เขมินท์ ศิริขจร

--- Seven_Dwarves/src/Seven_Dwarves.java
+++ Seven_Dwarves/src/Seven_Dwarves.java
@@ -1,117 +1,116 @@
  import java.util.*; 
  public class Seven_Dwarves {  
  public static void main(String[] args) { 	
-	 int k1,k2,k3,k4,k5,k6,k7,k8,k9; 	
+	 int d1,d2,d3,d4,d5,d6,d7,d8,d9; 	
 	 int sum=0,over=0;  	
 	 
 	 Scanner scan=new Scanner(System.in); 	
 do{ 			
-	k1=scan.nextInt(); 		if(k1>99||k1<0) 	
-		System.out.println("number 1. Error!"); 	
-	}while(k1>99||k1<0); 
+	d1=scan.nextInt(); 		if(d1>99||d1<0) 	
+		System.out.println("number 1 not Dwarf!"); 	
+	}while(d1>99||d1<0); 
 
 do{ 			 
-	k2=scan.nextInt(); 		if(k2>99||k2<0) 
-		System.out.println("number 2. Error!"); 	
-	}while(k2>99||k2<0); 	
+	d2=scan.nextInt(); 		if(d2>99||d2<0) 
+		System.out.println("number 2 not Dwarf!"); 	
+	}while(d2>99||d2<0); 	
 
 do{ 
-k3=scan.nextInt(); 		if(k3>99||k3<0) 	
-	System.out.println("number 3. Error!"); 
-}while(k3>99||k3<0); 	
+	d3=scan.nextInt(); 		if(d3>99||d3<0) 	
+	System.out.println("number 3 not Dwarf!"); 
+}while(d3>99||d3<0); 	
 
 do{ 		
-	k4=scan.nextInt(); 
-if(k4>99||k4<0) 	
-	System.out.println("number 4. Error!"); 	
-}while(k4>99||k4<0); 	
+	d4=scan.nextInt(); 
+if(d4>99||d4<0) 	
+	System.out.println("number 4 not Dwarf!"); 	
+}while(d4>99||d4<0); 	
 
 do{ 	
-	k5=scan.nextInt(); 	
-	if(k5>99||k5<0) 
-		System.out.println("number 5. Error!"); 
-	}while(k5>99||k5<0); 
+	d5=scan.nextInt(); 	
+	if(d5>99||d5<0) 
+		System.out.println("number 5 not Dwarf!"); 
+	}while(d5>99||d5<0); 
 
 do{ 	
-	k6=scan.nextInt(); 
-if(k6>99||k6<0) 	
-	System.out.println("number 6. Error!"); 
-}while(k6>99||k6<0); 	
+	d6=scan.nextInt(); 
+if(d6>99||d6<0) 	
+	System.out.println("number 6 not Dwarf!"); 
+}while(d6>99||d6<0); 	
 
 
-do{ 		k7=scan.nextInt(); 	
-if(k7>99||k7<0) 	
-	System.out.println("number 7. Error!"); 	
-}while(k7>99||k7<0); 	
+do{ 		d7=scan.nextInt(); 	
+if(d7>99||d7<0) 	
+	System.out.println("number 7 not Dwarf!"); 	
+}while(d7>99||d7<0); 	
 
 
-do{ 		k8=scan.nextInt(); 
-if(k8>99||k8<0) 
-	System.out.println("number 8. Error!"); 	
-}while(k8>99||k8<0); 
+do{ 		d8=scan.nextInt(); 
+if(d8>99||d8<0) 
+	System.out.println("number 8 not Dwarf!"); 	
+}while(d8>99||d8<0); 
 
-do{ 		k9=scan.nextInt(); 	
-if(k9>99||k9<0) 	
-	System.out.println("number 9. Error!"); 
-}while(k9>99||k9<0); 	
+do{ 		d9=scan.nextInt(); 	
+if(d9>99||d9<0) 	
+	System.out.println("number 9 not Dwarf!"); 
+}while(d9>99||d9<0); 	
 
 
-sum=k1+k2+k3+k4+k5+k6+k7+k8+k9; 	
+sum=d1+d2+d3+d4+d5+d6+d7+d8+d9; 	
 over=sum-100;  	
 
 
-	if(k1+k2==over)k1=k2=0; 	
-else if(k1+k3==over)k1=k3=0; 	
-else if(k1+k4==over)k1=k4=0; 		
-else if(k1+k5==over)k1=k5=0; 		
-else if(k1+k6==over)k1=k6=0; 		
-else if(k1+k7==over)k1=k7=0; 		
-else if(k1+k8==over)k1=k8=0; 		
-else if(k1+k9==over)k1=k9=0; 		 		
-else if(k2+k3==over)k2=k3=0; 		
-else if(k2+k4==over)k2=k4=0; 		
-else if(k2+k5==over)k2=k5=0; 		
-else if(k2+k6==over)k2=k6=0; 		
-else if(k2+k7==over)k2=k7=0; 		
-else if(k2+k8==over)k2=k8=0; 		
-else if(k2+k9==over)k2=k9=0; 		 		
-else if(k3+k4==over)k3=k4=0; 		
-else if(k3+k5==over)k3=k5=0; 		
-else if(k3+k6==over)k3=k6=0; 		
-else if(k3+k7==over)k3=k7=0; 		
-else if(k3+k8==over)k3=k8=0; 		
-else if(k3+k9==over)k3=k9=0; 		 	
-else if(k4+k5==over)k4=k5=0; 	
-else if(k4+k6==over)k4=k6=0; 	
-else if(k4+k7==over)k4=k7=0; 	
-else if(k4+k8==over)k4=k8=0; 	
-else if(k4+k9==over)k4=k9=0; 		
-else if(k5+k6==over)k5=k6=0; 		
-else if(k5+k7==over)k5=k7=0; 		
-else if(k5+k8==over)k5=k8=0; 		
-else if(k5+k9==over)k5=k9=0; 		
-else if(k6+k7==over)k6=k7=0; 		
-else if(k6+k8==over)k6=k8=0; 		
-else if(k6+k9==over)k6=k9=0; 		
-else if(k7+k8==over)k7=k8=0; 		
-else if(k7+k9==over)k7=k9=0; 		
-else if(k8+k9==over)k8=k9=0; 		
+	if(d1+d2==over)d1=d2=0; 	
+else if(d1+d3==over)d1=d3=0; 	
+else if(d1+d4==over)d1=d4=0; 		
+else if(d1+d5==over)d1=d5=0; 		
+else if(d1+d6==over)d1=d6=0; 		
+else if(d1+d7==over)d1=d7=0; 		
+else if(d1+d8==over)d1=d8=0; 		
+else if(d1+d9==over)d1=d9=0; 		 		
+else if(d2+d3==over)d2=d3=0; 		
+else if(d2+d4==over)d2=d4=0; 		
+else if(d2+d5==over)d2=d5=0; 		
+else if(d2+d6==over)d2=d6=0; 		
+else if(d2+d7==over)d2=d7=0; 		
+else if(d2+d8==over)d2=d8=0; 		
+else if(d2+d9==over)d2=d9=0; 		 		
+else if(d3+d4==over)d3=d4=0; 		
+else if(d3+d5==over)d3=d5=0; 		
+else if(d3+d6==over)d3=d6=0; 		
+else if(d3+d7==over)d3=d7=0; 		
+else if(d3+d8==over)d3=d8=0; 		
+else if(d3+d9==over)d3=d9=0; 		 	
+else if(d4+d5==over)d4=d5=0; 	
+else if(d4+d6==over)d4=d6=0; 	
+else if(d4+d7==over)d4=d7=0; 	
+else if(d4+d8==over)d4=d8=0; 	
+else if(d4+d9==over)d4=d9=0; 		
+else if(d5+d6==over)d5=d6=0; 		
+else if(d5+d7==over)d5=d7=0; 		
+else if(d5+d8==over)d5=d8=0; 		
+else if(d5+d9==over)d5=d9=0; 		
+else if(d6+d7==over)d6=d7=0; 		
+else if(d6+d8==over)d6=d8=0; 		
+else if(d6+d9==over)d6=d9=0; 		
+else if(d7+d8==over)d7=d8=0; 		
+else if(d7+d9==over)d7=d9=0; 		
+else if(d8+d9==over)d8=d9=0; 		
 	
 	
-else { 
- 
- System.out.println("Your Enter number Error"); 		
-	k1=k2=k3=k4=k5=k6=k7=k8=k9=0; 
-}	 	
-	if (k1!=0)System.out.println(k1); 
-	if (k2!=0)	System.out.println(k2); 	
-	if (k3!=0)System.out.println(k3); 	
-	if (k4!=0)	System.out.println(k4); 	
-	if (k5!=0)System.out.println(k5); 	
-	if (k6!=0)System.out.println(k6); 	
-	if (k7!=0)System.out.println(k7); 
-	if (k8!=0)System.out.println(k8); 	
-	if (k9!=0)System.out.println(k9); 		 		 
+else { 	
+	System.out.println("Your Enter number "); 		
+	d1=d2=d3=d4=d5=d6=d7=d8=d9=0; 
+	} 		 	
+	if (d1!=0)System.out.println(d1); 
+	if (d2!=0)	System.out.println(d2); 	
+	if (d3!=0)System.out.println(d3); 	
+	if (d4!=0)	System.out.println(d4); 	
+	if (d5!=0)System.out.println(d5); 	
+	if (d6!=0)System.out.println(d6); 	
+	if (d7!=0)System.out.println(d7); 
+	if (d8!=0)System.out.println(d8); 	
+	if (d9!=0)System.out.println(d9); 		 		 
 } 	 
 }
 
